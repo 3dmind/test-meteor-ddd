a74efa08e11be4e81955724341c5da95613c67b9
Christian Wlatschiha <c.wlatschiha@gmail.com>
2019-09-25 11:39:29 +0200

    chore: Project setup.

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  printWidth: 80,
+  useTabs: false,
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  arrowParens: 'always',
+}
