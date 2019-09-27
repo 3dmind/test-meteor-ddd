9648c0ad5db6c5779464c79fcc55d8dd250577ea
Christian Wlatschiha <c.wlatschiha@gmail.com>
2019-09-27 20:17:02 +0200

    feat: Map task entity to persistence and back.

---
+++ imports/planning/task/TaskMapper.ts
@@ -0,0 +1,47 @@
+import { UniqueEntityId } from '../../core/domain/UniqueEntityId'
+import { TaskDocument } from './api/TasksCollection'
+import { TaskEntity } from './domain/TaskEntity'
+import { TaskDescription } from './domain/TaskDescription'
+
+export const TaskMapper = {
+  toPersistence(task: TaskEntity): TaskDocument {
+    const document: TaskDocument = {
+      description: task.description.value,
+      createdAt: task.createdAt,
+      editedAt: task.editedAt,
+      isTickedOff: task.isTickedOff(),
+      tickedOffAt: task.tickedOffAt,
+      resumedAt: task.resumedAt,
+      isDiscarded: task.isDiscarded(),
+      discardedAt: task.discardedAt,
+      isArchived: task.isArchived(),
+      archivedAt: task.archivedAt,
+    }
+
+    if (task.id) {
+      Object.assign(document, { _id: task.id.value })
+    }
+
+    return document
+  },
+
+  toDomain(doc: TaskDocument): TaskEntity {
+    return TaskEntity.create(
+      {
+        description: TaskDescription.create(doc.description),
+        createdAt: doc.createdAt,
+        resumedAt: doc.resumedAt,
+        tickedOff: doc.isTickedOff,
+        tickedOffAt: doc.tickedOffAt,
+        editedAt: doc.editedAt,
+        discarded: doc.isDiscarded,
+        discardedAt: doc.discardedAt,
+        archived: doc.isArchived,
+        archivedAt: doc.archivedAt,
+      },
+      UniqueEntityId.create(doc._id),
+    )
+  },
+}
+
+Object.freeze(TaskMapper)
