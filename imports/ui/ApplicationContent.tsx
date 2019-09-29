c8822d8922b1ccb1261d6bdd4d2181cd27eaee8a
Christian Wlatschiha <c.wlatschiha@gmail.com>
2019-09-30 00:14:50 +0200

    feat(ui): Application layout.

---
+++ imports/ui/ApplicationContent.tsx
@@ -0,0 +1,22 @@
+import { createStyles, makeStyles, Theme } from '@material-ui/core'
+import Container from '@material-ui/core/Container'
+import * as React from 'react'
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      boxSizing: 'border-box',
+      padding: theme.spacing(4, 0, 0, 0),
+    },
+  }),
+)
+
+export const ApplicationContent: React.FunctionComponent = (props) => {
+  const { children } = props
+  const classes = useStyles(props)
+  return (
+    <main className={classes.root}>
+      <Container maxWidth={'xl'}>{children}</Container>
+    </main>
+  )
+}
