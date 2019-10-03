65a81831f49569675e9016a8826a076185f2c46f
Christian Wlatschiha <c.wlatschiha@gmail.com>
2019-10-03 22:12:59 +0200

    test(api): Add tests for task collection and methods.

---
+++ tests/server/planning/task/api/TasksCollection.test.ts
@@ -0,0 +1,19 @@
+import * as assert from 'assert'
+import { taskPublicFields } from '../../../../../imports/planning/task/api/TasksCollection'
+
+describe('TasksCollection', function() {
+  it('#taskPublicFields', function() {
+    assert.deepStrictEqual(taskPublicFields, {
+      description: 1,
+      createdAt: 1,
+      editedAt: 1,
+      isTickedOff: 1,
+      tickedOffAt: 1,
+      resumedAt: 1,
+      isDiscarded: 1,
+      discardedAt: 1,
+      isArchived: 1,
+      archivedAt: 1,
+    })
+  })
+})
