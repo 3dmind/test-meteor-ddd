09dd74ea104706675a64367db0fa9defd7cdea63
Christian Wlatschiha <c.wlatschiha@gmail.com>
2019-10-04 17:03:25 +0200

    test(api): Discard all archived tasks at once.

--- tests/server/planning/task/api/TaskMethods.test.ts
+++ tests/server/planning/task/api/TaskMethods.test.ts
@@ -8,6 +8,7 @@ import {
 } from '../../../../../imports/planning/task/api/TasksCollection'
 import {
   PLANNING_TASK_ARCHIVE_METHOD,
+  PLANNING_TASK_DISCARD_ALL_ARCHIVE_METHOD,
   PLANNING_TASK_DISCARD_METHOD,
   PLANNING_TASK_EDIT_METHOD,
   PLANNING_TASK_NOTE_METHOD,
@@ -16,12 +17,14 @@ import {
 } from '../../../../../imports/planning/task/constants'
 import {
   ArchiveTaskDto,
+  DiscardArchivedTasksDto,
   DiscardTaskDto,
   EditTaskDto,
   NoteTaskDto,
   ResumeTaskDto,
   TickOffTaskDto,
 } from '../../../../../imports/planning/task/dto'
+import { TaskUiModel } from '../../../../../imports/planning/task/ui/TaskUiModel'
 
 if (Meteor.isServer) {
   describe('TaskMethods', function() {
@@ -97,6 +100,27 @@ if (Meteor.isServer) {
       const archiveTaskDto = new ArchiveTaskDto(taskDocumentId)
 
       archiveTaskMethodHandler.apply({}, [archiveTaskDto])
+      const actual = TasksCollection.find(selector).count()
+
+      assert.strictEqual(actual, 1)
+    })
+
+    it('discard all archived tasks', function() {
+      const archivedTaskDocument = Object.assign({}, taskDocument, {
+        isArchived: true,
+      })
+      const taskDocumentId = TasksCollection.insert(archivedTaskDocument)
+      const selector: Mongo.Selector<TaskDocument> = { isDiscarded: true }
+      const methodHandler =
+        Meteor.server.method_handlers[PLANNING_TASK_DISCARD_ALL_ARCHIVE_METHOD]
+      const taskUiModel: TaskUiModel = {
+        taskId: taskDocumentId,
+        description: archivedTaskDocument.description,
+        isTickedOff: archivedTaskDocument.isTickedOff,
+      }
+      const discardArchivedTasksDto = new DiscardArchivedTasksDto([taskUiModel])
+
+      methodHandler.apply({}, [discardArchivedTasksDto])
       const actual = TasksCollection.find(selector).count()
 
       assert.strictEqual(actual, 1)
