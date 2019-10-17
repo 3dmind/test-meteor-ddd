d1b1bad9374a592a0bd6238030638601acfc653c
Christian Wlatschiha <c.wlatschiha@gmail.com>
2019-10-17 21:53:17 +0200

    feat: Typed Meteor method call with DTO contract.

--- typings/meteor.d.ts
+++ typings/meteor.d.ts
@@ -1,7 +1,17 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 declare module 'meteor/meteor' {
   namespace Meteor {
     const server: {
       method_handlers: {}
     }
+
+    function call<DTO>(
+      name: string,
+      dto: DTO,
+      callback?: (
+        error: Error | Meteor.Error | undefined,
+        result?: any,
+      ) => void,
+    ): any
   }
 }
