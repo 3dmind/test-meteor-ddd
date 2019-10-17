fdad04ac895529a36b6defff8e1a08da0140e9fc
Christian Wlatschiha <c.wlatschiha@gmail.com>
2019-10-17 21:54:40 +0200

    refactor(ui): Use typed Meteor method call with DTO contract.
    Closes test-meteor-ddd-8

--- imports/planning/task/ui/actions/archiveTaskAction.ts
+++ imports/planning/task/ui/actions/archiveTaskAction.ts
@@ -5,12 +5,15 @@ import { TaskUiModel } from '../models'
 
 export function archiveTaskAction(task: TaskUiModel): Promise<void> {
   return new Promise((resolve, reject): void => {
-    const dto: TaskDto = { taskId: task.id }
-    Meteor.call(PLANNING_TASK_ARCHIVE_METHOD, dto, (error, value) => {
-      if (error) {
-        reject(error)
-      }
-      resolve(value)
-    })
+    Meteor.call<TaskDto>(
+      PLANNING_TASK_ARCHIVE_METHOD,
+      { taskId: task.id },
+      (error, value) => {
+        if (error) {
+          reject(error)
+        }
+        resolve(value)
+      },
+    )
   })
 }
