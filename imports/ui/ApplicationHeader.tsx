9f68bf0c4e57abe26bfed80d86022439492bd626
Christian Wlatschiha <c.wlatschiha@gmail.com>
2019-11-13 00:49:55 +0100

    feat(ui): Create user account and login.

--- imports/ui/ApplicationHeader.tsx
+++ imports/ui/ApplicationHeader.tsx
@@ -1,15 +1,28 @@
+import { createStyles, makeStyles } from '@material-ui/core'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
 import * as React from 'react'
+import { AccountsUiWrapper } from './AccountsUiWrapper'
 
-export const ApplicationHeader: React.FunctionComponent = () => {
+const useStyles = makeStyles(() =>
+  createStyles({
+    spacer: {
+      flexGrow: 1,
+    },
+  }),
+)
+
+export const ApplicationHeader: React.FunctionComponent = (props) => {
+  const classes = useStyles(props)
   return (
     <AppBar position={'relative'}>
       <Toolbar>
         <Typography component={'h1'} variant={'h5'}>
           Domain-Driven Design with Meteor
         </Typography>
+        <div className={classes.spacer} />
+        <AccountsUiWrapper />
       </Toolbar>
     </AppBar>
   )
