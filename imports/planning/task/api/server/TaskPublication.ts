2799fe2244e117e78a64e478f84414828ee7e2de
Christian Wlatschiha <c.wlatschiha@gmail.com>
2019-11-17 00:50:54 +0100

    feat(api): Only publish tasks that belong to the current user.

--- imports/planning/task/api/server/TaskPublication.ts
+++ imports/planning/task/api/server/TaskPublication.ts
@@ -1,9 +1,17 @@
 import { Meteor } from 'meteor/meteor'
+import { Mongo } from 'meteor/mongo'
 import { PublicationsEnum } from '../../enums'
-import { TaskCollection, taskPublicFields } from '../TaskCollection'
+import {
+  TaskCollection,
+  TaskDocument,
+  taskPublicFields,
+} from '../TaskCollection'
 
 Meteor.publish(PublicationsEnum.Tasks, function tasksPublication() {
-  const selector = { isDiscarded: false }
+  const selector: Mongo.Selector<TaskDocument> = {
+    ownerId: this.userId,
+    isDiscarded: false,
+  }
   const options = { fields: taskPublicFields }
   return TaskCollection.find(selector, options)
 })
