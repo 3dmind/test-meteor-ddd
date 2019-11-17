022e45add234773a699116fb2f08c1bfa4d96c4d
Christian Wlatschiha <c.wlatschiha@gmail.com>
2019-11-17 15:36:16 +0100

    feat: Only the owner can discard a task.

--- tests/server/planning/task/api/methods/discardTaskMethod.test.ts
+++ tests/server/planning/task/api/methods/discardTaskMethod.test.ts
@@ -2,6 +2,11 @@ import * as assert from 'assert'
 import { Meteor } from 'meteor/meteor'
 import { Mongo } from 'meteor/mongo'
 import {
+  TaskNotFoundException,
+  UnauthorizedMethodCallException,
+  UnauthorizedTaskOperationException,
+} from '../../../../../../imports/planning/task/api/exceptions'
+import {
   TaskCollection,
   TaskDocument,
 } from '../../../../../../imports/planning/task/api/TaskCollection'
@@ -10,19 +15,55 @@ import { MethodNamesEnum } from '../../../../../../imports/planning/task/enums'
 import { taskDocFixture, userIdFixture } from './fixtures'
 
 describe('Discard task method', function() {
+  let discardTaskMethod
+  let documentId
+
+  before(function() {
+    discardTaskMethod =
+      Meteor.server.method_handlers[MethodNamesEnum.DiscardTask]
+  })
+
   beforeEach(function() {
+    documentId = TaskCollection.insert(taskDocFixture)
+  })
+
+  afterEach(function() {
     TaskCollection.remove({})
   })
 
-  it('discard task', function() {
-    const documentId = TaskCollection.insert(taskDocFixture)
+  it('should throw when user is not logged-in', function() {
+    const context = {}
+    const dto: TaskDto = { taskId: documentId }
+
+    assert.throws(() => {
+      discardTaskMethod.apply(context, [dto])
+    }, UnauthorizedMethodCallException)
+  })
+
+  it('should throw when task was not found', function() {
+    const context = Object.assign({}, { userId: userIdFixture })
+    const dto: TaskDto = { taskId: 'A' }
+
+    assert.throws(() => {
+      discardTaskMethod.apply(context, [dto])
+    }, TaskNotFoundException)
+  })
+
+  it('should throw when owner and user do not match', function() {
+    const context = Object.assign({}, { userId: 'YanhGvrizEdDzqQEz' })
+    const dto: TaskDto = { taskId: documentId }
+
+    assert.throws(() => {
+      discardTaskMethod.apply(context, [dto])
+    }, UnauthorizedTaskOperationException)
+  })
+
+  it('should discard task', function() {
     const selector: Mongo.Selector<TaskDocument> = { isDiscarded: true }
-    const methodHandler =
-      Meteor.server.method_handlers[MethodNamesEnum.DiscardTask]
     const context = Object.assign({}, { userId: userIdFixture })
     const dto: TaskDto = { taskId: documentId }
 
-    methodHandler.apply(context, [dto])
+    discardTaskMethod.apply(context, [dto])
     const actual = TaskCollection.find(selector).count()
 
     assert.strictEqual(actual, 1)
