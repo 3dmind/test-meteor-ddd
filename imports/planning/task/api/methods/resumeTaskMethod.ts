6174d727a8ac32c6ddc18e590b70504a29b2ddc0
Christian Wlatschiha <c.wlatschiha@gmail.com>
2019-11-17 17:51:35 +0100

    feat: Only the owner can resume a task.

--- imports/planning/task/api/methods/resumeTaskMethod.ts
+++ imports/planning/task/api/methods/resumeTaskMethod.ts
@@ -1,14 +1,26 @@
 import { Meteor } from 'meteor/meteor'
+import { UniqueId } from '../../../../core/domain'
 import { TaskDto } from '../../dto'
 import { MethodNamesEnum } from '../../enums'
-import { TaskNotFoundException } from '../exceptions'
+import {
+  TaskNotFoundException,
+  UnauthorizedMethodCallException,
+  UnauthorizedTaskOperationException,
+} from '../exceptions'
 import { TaskRepository } from '../TaskRepository'
 
 Meteor.methods({
   [MethodNamesEnum.ResumeTask]: function resumeTaskMethod(dto: TaskDto): void {
+    if (!this.userId) {
+      throw new UnauthorizedMethodCallException()
+    }
+
     const task = TaskRepository.getTaskById(dto.taskId)
     if (!task) {
       throw new TaskNotFoundException()
+    }
+    if (!task.isOwnedByUser(UniqueId.create(this.userId))) {
+      throw new UnauthorizedTaskOperationException()
     }
     task.resume()
     TaskRepository.updateTask(task)
--- tests/server/planning/task/api/methods/resumeTaskMethod.test.ts
+++ tests/server/planning/task/api/methods/resumeTaskMethod.test.ts
@@ -2,6 +2,11 @@ import * as assert from 'assert'
 import { Meteor } from 'meteor/meteor'
 import { Mongo } from 'meteor/mongo'
 import {
+  TaskNotFoundException,
+  UnauthorizedMethodCallException,
+  UnauthorizedTaskOperationException,
+} from '../../../../../../imports/planning/task/api/exceptions'
+import {
   TaskCollection,
   TaskDocument,
 } from '../../../../../../imports/planning/task/api/TaskCollection'
@@ -10,20 +15,55 @@ import { MethodNamesEnum } from '../../../../../../imports/planning/task/enums'
 import { taskDocFixture, userIdFixture } from './fixtures'
 
 describe('Resume task method', function() {
+  let resumeTaskMethod
+  let documentId
+
+  before(function() {
+    resumeTaskMethod = Meteor.server.method_handlers[MethodNamesEnum.ResumeTask]
+  })
+
   beforeEach(function() {
+    const document = Object.assign({}, taskDocFixture, { isTickedOff: true })
+    documentId = TaskCollection.insert(document)
+  })
+
+  afterEach(function() {
     TaskCollection.remove({})
   })
 
-  it('resume task', function() {
-    const assign = Object.assign({}, taskDocFixture, { isTickedOff: true })
-    const documentId = TaskCollection.insert(assign)
+  it('should throw when user is not logged-in', function() {
+    const context = {}
+    const dto: TaskDto = { taskId: documentId }
+
+    assert.throws(() => {
+      resumeTaskMethod.apply(context, [dto])
+    }, UnauthorizedMethodCallException)
+  })
+
+  it('should throw when task was not found', function() {
+    const context = Object.assign({}, { userId: userIdFixture })
+    const dto: TaskDto = { taskId: 'A' }
+
+    assert.throws(() => {
+      resumeTaskMethod.apply(context, [dto])
+    }, TaskNotFoundException)
+  })
+
+  it('should throw when owner and user do not match', function() {
+    const context = Object.assign({}, { userId: 'YanhGvrizEdDzqQEz' })
+    const dto: TaskDto = { taskId: documentId }
+
+    assert.throws(() => {
+      resumeTaskMethod.apply(context, [dto])
+    }, UnauthorizedTaskOperationException)
+  })
+
+  it('should resume task', function() {
     const selector: Mongo.Selector<TaskDocument> = { isTickedOff: false }
-    const methodHandler =
-      Meteor.server.method_handlers[MethodNamesEnum.ResumeTask]
     const context = Object.assign({}, { userId: userIdFixture })
     const dto: TaskDto = { taskId: documentId }
 
-    methodHandler.apply(context, [dto])
+    resumeTaskMethod.apply(context, [dto])
     const actual = TaskCollection.find(selector).count()
 
     assert.strictEqual(actual, 1)
