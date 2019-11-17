abd08647260536dfd6ed1a53f4ade37029b72b95
Christian Wlatschiha <c.wlatschiha@gmail.com>
2019-11-17 15:45:15 +0100

    feat: Only the owner can edit a task.

--- imports/planning/task/api/methods/editTaskMethod.ts
+++ imports/planning/task/api/methods/editTaskMethod.ts
@@ -1,10 +1,12 @@
 import { Meteor } from 'meteor/meteor'
+import { UniqueId } from '../../../../core/domain'
 import { TaskDescription } from '../../domain/TaskDescription'
 import { EditTaskDto } from '../../dto'
 import { MethodNamesEnum } from '../../enums'
 import {
   TaskNotFoundException,
   UnauthorizedMethodCallException,
+  UnauthorizedTaskOperationException,
 } from '../exceptions'
 import { TaskRepository } from '../TaskRepository'
 
@@ -17,6 +19,9 @@ Meteor.methods({
     const task = TaskRepository.getTaskById(dto.taskId)
     if (!task) {
       throw new TaskNotFoundException()
+    }
+    if (!task.isOwnedByUser(UniqueId.create(this.userId))) {
+      throw new UnauthorizedTaskOperationException()
     }
     const newTaskDescription = TaskDescription.create(dto.newText)
     task.edit(newTaskDescription)
--- tests/server/planning/task/api/methods/editTaskMethod.test.ts
+++ tests/server/planning/task/api/methods/editTaskMethod.test.ts
@@ -2,6 +2,11 @@ import * as assert from 'assert'
 import { Meteor } from 'meteor/meteor'
 import { Mongo } from 'meteor/mongo'
 import {
+  TaskNotFoundException,
+  UnauthorizedMethodCallException,
+  UnauthorizedTaskOperationException,
+} from '../../../../../../imports/planning/task/api/exceptions'
+import {
   TaskCollection,
   TaskDocument,
 } from '../../../../../../imports/planning/task/api/TaskCollection'
@@ -10,23 +15,70 @@ import { MethodNamesEnum } from '../../../../../../imports/planning/task/enums'
 import { taskDocFixture, userIdFixture } from './fixtures'
 
 describe('Edit task method', function() {
+  let editTaskMethod
+  let documentId
+
+  before(function() {
+    editTaskMethod = Meteor.server.method_handlers[MethodNamesEnum.EditTask]
+  })
+
   beforeEach(function() {
+    documentId = TaskCollection.insert(taskDocFixture)
+  })
+
+  afterEach(function() {
     TaskCollection.remove({})
   })
 
-  it('edit task description', function() {
-    const documentId = TaskCollection.insert(taskDocFixture)
+  it('should throw when user is not logged-in', function() {
+    const newText = 'Lorem ispum dolor amet sum'
+    const context = {}
+    const dto: EditTaskDto = {
+      taskId: documentId,
+      newText,
+    }
+
+    assert.throws(() => {
+      editTaskMethod.apply(context, [dto])
+    }, UnauthorizedMethodCallException)
+  })
+
+  it('should throw when task was not found', function() {
+    const newText = 'Lorem ispum dolor amet sum'
+    const context = Object.assign({}, { userId: userIdFixture })
+    const dto: EditTaskDto = {
+      taskId: 'A',
+      newText,
+    }
+
+    assert.throws(() => {
+      editTaskMethod.apply(context, [dto])
+    }, TaskNotFoundException)
+  })
+
+  it('should throw when owner and user do not match', function() {
+    const newText = 'Lorem ispum dolor amet sum'
+    const context = Object.assign({}, { userId: 'YanhGvrizEdDzqQEz' })
+    const dto: EditTaskDto = {
+      taskId: documentId,
+      newText,
+    }
+
+    assert.throws(() => {
+      editTaskMethod.apply(context, [dto])
+    }, UnauthorizedTaskOperationException)
+  })
+
+  it('should edit task description', function() {
     const newText = 'Lorem ispum dolor amet sum'
     const selector: Mongo.Selector<TaskDocument> = { description: newText }
-    const methodHandler =
-      Meteor.server.method_handlers[MethodNamesEnum.EditTask]
     const context = Object.assign({}, { userId: userIdFixture })
     const dto: EditTaskDto = {
       taskId: documentId,
       newText,
     }
 
-    methodHandler.apply(context, [dto])
+    editTaskMethod.apply(context, [dto])
     const actual = TaskCollection.find(selector).count()
 
     assert.strictEqual(actual, 1)
