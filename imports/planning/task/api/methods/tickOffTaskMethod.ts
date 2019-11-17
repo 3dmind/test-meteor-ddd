35cc510dd3459f5f5b986618b0dc307115964045
Christian Wlatschiha <c.wlatschiha@gmail.com>
2019-11-17 17:38:06 +0100

    feat: Only the owner can tick-off a task.

--- imports/planning/task/api/methods/tickOffTaskMethod.ts
+++ imports/planning/task/api/methods/tickOffTaskMethod.ts
@@ -1,16 +1,28 @@
 import { Meteor } from 'meteor/meteor'
+import { UniqueId } from '../../../../core/domain'
 import { TaskDto } from '../../dto'
 import { MethodNamesEnum } from '../../enums'
-import { TaskNotFoundException } from '../exceptions'
+import {
+  TaskNotFoundException,
+  UnauthorizedMethodCallException,
+  UnauthorizedTaskOperationException,
+} from '../exceptions'
 import { TaskRepository } from '../TaskRepository'
 
 Meteor.methods({
   [MethodNamesEnum.TickOffTask]: function tickOffTaskMethod(
     dto: TaskDto,
   ): void {
+    if (!this.userId) {
+      throw new UnauthorizedMethodCallException()
+    }
+
     const task = TaskRepository.getTaskById(dto.taskId)
     if (!task) {
       throw new TaskNotFoundException()
+    }
+    if (!task.isOwnedByUser(UniqueId.create(this.userId))) {
+      throw new UnauthorizedTaskOperationException()
     }
     task.tickOff()
     TaskRepository.updateTask(task)
