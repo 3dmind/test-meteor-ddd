289e692390fb1ec2ebbc1a32fb510d9fb770e657
Christian Wlatschiha <c.wlatschiha@gmail.com>
2019-11-25 00:24:50 +0100

    test(api): Add missing tests.

--- tests/server/planning/task/api/methods/discardAllArchivedTasksMethod.test.ts
+++ tests/server/planning/task/api/methods/discardAllArchivedTasksMethod.test.ts
@@ -2,6 +2,10 @@ import * as assert from 'assert'
 import { Meteor } from 'meteor/meteor'
 import { Mongo } from 'meteor/mongo'
 import {
+  TaskNotFoundException,
+  UnauthorizedMethodCallException,
+} from '../../../../../../imports/planning/task/api/exceptions'
+import {
   TaskCollection,
   TaskDocument,
 } from '../../../../../../imports/planning/task/api/TaskCollection'
@@ -10,24 +14,55 @@ import { MethodNamesEnum } from '../../../../../../imports/planning/task/enums'
 import { taskDocFixture, userIdFixture } from './fixtures'
 
 describe('Discard all archived tasks method', function() {
-  beforeEach(function() {
-    TaskCollection.remove({})
+  let discardAllArchivedTasksMethod
+  let documentId
+
+  before(function() {
+    discardAllArchivedTasksMethod =
+      Meteor.server.method_handlers[MethodNamesEnum.DiscardAllArchivedTasks]
   })
 
-  it('discard all archived tasks', function() {
+  beforeEach(function() {
     const archivedTaskDocument = Object.assign({}, taskDocFixture, {
       isArchived: true,
     })
-    const documentId = TaskCollection.insert(archivedTaskDocument)
+    documentId = TaskCollection.insert(archivedTaskDocument)
+  })
+
+  afterEach(function() {
+    TaskCollection.remove({})
+  })
+
+  it('should throw when user is not logged-in', function() {
+    const context = {}
+    const dto: DiscardArchivedTasksDto = {
+      taskIds: [documentId],
+    }
+
+    assert.throws(() => {
+      discardAllArchivedTasksMethod.apply(context, [dto])
+    }, UnauthorizedMethodCallException)
+  })
+
+  it('should throw when no tasks were found', function() {
+    const context = Object.assign({}, { userId: userIdFixture })
+    const dto: DiscardArchivedTasksDto = {
+      taskIds: ['A'],
+    }
+
+    assert.throws(() => {
+      discardAllArchivedTasksMethod.apply(context, [dto])
+    }, TaskNotFoundException)
+  })
+
+  it('should discard all archived tasks', function() {
     const selector: Mongo.Selector<TaskDocument> = { isDiscarded: true }
-    const methodHandler =
-      Meteor.server.method_handlers[MethodNamesEnum.DiscardAllArchivedTasks]
     const context = Object.assign({}, { userId: userIdFixture })
     const dto: DiscardArchivedTasksDto = {
       taskIds: [documentId],
     }
 
-    methodHandler.apply(context, [dto])
+    discardAllArchivedTasksMethod.apply(context, [dto])
     const actual = TaskCollection.find(selector).count()
 
     assert.strictEqual(actual, 1)
