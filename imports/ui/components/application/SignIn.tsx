64c199e91a8148f14b0a85d6a440fcc1fe818558
Christian Wlatschiha <c.wlatschiha@gmail.com>
2019-12-03 01:32:51 +0100

    refactor(ui): Proper styling for sign in.

--- imports/ui/components/application/SignIn.tsx
+++ imports/ui/components/application/SignIn.tsx
@@ -1,15 +1,27 @@
 import {
   Button,
   Container,
+  createStyles,
   Grid,
+  makeStyles,
+  Paper,
   TextField,
   Typography,
 } from '@material-ui/core'
 import * as React from 'react'
-import { useHistory } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
 import { useActions } from '../ApplicationActions'
 
-export const SignIn: React.FunctionComponent = () => {
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    paper: {
+      marginTop: theme.spacing(8),
+      padding: theme.spacing(3),
+    },
+  }),
+)
+
+export const SignIn: React.FunctionComponent = (props) => {
   const history = useHistory()
   const { signInAction } = useActions()
   const [username, setUsername] = React.useState<string>('')
@@ -43,46 +55,60 @@ export const SignIn: React.FunctionComponent = () => {
       .catch(handleRejected)
   }
 
+  const classes = useStyles(props)
   return (
     <Container component={'main'} maxWidth={'xs'}>
-      <Typography component={'h1'} variant={'h5'}>
-        Login
-      </Typography>
-      <form noValidate onSubmit={handleSubmit}>
-        <Grid container spacing={2}>
-          <Grid item xs={12}>
-            <TextField
-              fullWidth
-              variant={'outlined'}
-              name={'username'}
-              label={'Username'}
-              value={username}
-              onChange={handleChangeUsername}
-            />
+      <Paper elevation={5} className={classes.paper}>
+        <Grid container spacing={2} direction={'column'}>
+          <Grid item>
+            <Typography component={'h1'} variant={'h5'} align={'center'}>
+              Login
+            </Typography>
           </Grid>
-          <Grid item xs={12}>
-            <TextField
-              fullWidth
-              variant={'outlined'}
-              name={'password'}
-              label={'Password'}
-              type={'password'}
-              value={password}
-              onChange={handleChangePassword}
-            />
+          <Grid item>
+            <form noValidate onSubmit={handleSubmit}>
+              <Grid container spacing={2} direction={'column'}>
+                <Grid item xs={12}>
+                  <TextField
+                    fullWidth
+                    variant={'outlined'}
+                    name={'username'}
+                    label={'Username'}
+                    value={username}
+                    onChange={handleChangeUsername}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    fullWidth
+                    variant={'outlined'}
+                    name={'password'}
+                    label={'Password'}
+                    type={'password'}
+                    value={password}
+                    onChange={handleChangePassword}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <Button
+                    fullWidth
+                    type={'submit'}
+                    variant={'contained'}
+                    color={'primary'}
+                  >
+                    Sign in
+                  </Button>
+                </Grid>
+              </Grid>
+            </form>
           </Grid>
-          <Grid item xs={12}>
-            <Button
-              fullWidth
-              type={'submit'}
-              variant={'contained'}
-              color={'primary'}
-            >
-              Sign in
-            </Button>
+          <Grid item>
+            <Typography component={'p'} align={'right'}>
+              <Link to={'/signup'}>Don&apos;t have an account? Sign up</Link>
+            </Typography>
           </Grid>
         </Grid>
-      </form>
+      </Paper>
     </Container>
   )
 }
