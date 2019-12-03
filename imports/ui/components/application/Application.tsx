2386226eedb7c03721dfcb331944d873b421b71f
Christian Wlatschiha <c.wlatschiha@gmail.com>
2019-12-03 01:34:00 +0100

    feat(ui): Show sign-in when the user is not authenticated.

--- imports/ui/components/application/Application.tsx
+++ imports/ui/components/application/Application.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react'
+import { Route, Switch } from 'react-router-dom'
 import { TaskController } from '../../../planning/task/ui'
 import { ApplicationPresenter } from '../../presenter'
 import { ApplicationContent } from './ApplicationContent'
 import { ApplicationHeader } from './ApplicationHeader'
-import { Switch, Route } from 'react-router-dom'
 import { SignIn } from './SignIn'
 import { SignUp } from './SignUp'
 
@@ -20,9 +20,13 @@ export const Application: React.FunctionComponent<ApplicationProps> = (
       <ApplicationHeader isAuthenticated={application.isAuthenticated()} />
       <Switch>
         <Route exact path={'/'}>
-          <ApplicationContent>
-            <TaskController />
-          </ApplicationContent>
+          {application.isAuthenticated() ? (
+            <ApplicationContent>
+              <TaskController />
+            </ApplicationContent>
+          ) : (
+            <SignIn />
+          )}
         </Route>
         <Route path={'/login'}>
           <SignIn />
