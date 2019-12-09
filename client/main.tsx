0bff03c67d70aa21a744136fc14a02babe76a712
Christian Wlatschiha <c.wlatschiha@gmail.com>
2019-12-09 02:01:45 +0100

    refactor(ui): Accounts UI is replaced by custom solution.

--- client/main.tsx
+++ client/main.tsx
@@ -3,7 +3,6 @@ import { Meteor } from 'meteor/meteor'
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import { BrowserRouter as Router } from 'react-router-dom'
-import '../imports/startup/client/accounts.config'
 import { ApplicationController } from '../imports/ui'
 
 Meteor.startup(function onStartUp() {
