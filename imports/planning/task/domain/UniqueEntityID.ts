bcd6ba0ee8f9a61165a936eec35e368b0faa38ff
Christian Wlatschiha <c.wlatschiha@gmail.com>
2019-12-30 20:29:42 +0100

    refactor(domain): Task entity.

---
+++ imports/planning/task/domain/UniqueEntityID.ts
@@ -0,0 +1,30 @@
+import { Random } from 'meteor/random'
+import { isNil } from 'ramda'
+
+export class UniqueEntityID {
+  private readonly _value: string
+
+  private constructor(id: string) {
+    this._value = id
+  }
+
+  get value(): string {
+    return this._value
+  }
+
+  equals(id?: UniqueEntityID): boolean {
+    if (isNil(id)) {
+      return false
+    }
+
+    if (!(id instanceof UniqueEntityID)) {
+      return false
+    }
+
+    return id.value === this._value
+  }
+
+  static create(id?: string): UniqueEntityID {
+    return new UniqueEntityID(id ? id : Random.id())
+  }
+}
