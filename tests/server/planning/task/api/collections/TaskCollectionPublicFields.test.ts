9fe684eb5b508e21642135fca9276029e1fa29d4
Christian Wlatschiha <c.wlatschiha@gmail.com>
2020-02-02 21:02:47 +0100

    refactor:  Publish information of task discarding status.

--- tests/server/planning/task/api/collections/TaskCollectionPublicFields.test.ts
+++ tests/server/planning/task/api/collections/TaskCollectionPublicFields.test.ts
@@ -11,6 +11,8 @@ describe('TaskCollectionPublicFields', function() {
       isTickedOff: 1,
       tickedOffAt: 1,
       resumedAt: 1,
+      isDiscarded: 1,
+      discardedAt: 1,
       isArchived: 1,
       archivedAt: 1,
     });
